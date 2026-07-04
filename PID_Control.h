ba5a117cc1ad2617a043ac99db8e39270dfea920
KOJ-hashi <s.nitkagoshima@gmail.com>
2026-07-04 23:09:09 +0900

    Update PID_Control.h

--- PID_Control.h
+++ PID_Control.h
@@ -9,12 +9,13 @@ public:
         VELOCITY
     };
 
-    PID_Control(T kp, T ki, T kd, T dt, T min_out, T max_out, T alpha = 0.2f);
+    PID_Control(T kp, T ki, T kd, T dt, T min_out, T max_out, T alpha = 0.2f, T tolerance = 0);
 
     void set_mode(Mode mode);
-    void set_lpf_enabled(bool enabled);
-    void set_shortest_path(bool enabled); 
+    void set_lpf_enabled(bool enabled);//不確定微分
+    void set_shortest_path(bool enabled); //最短距離設定
     void set_alpha(T alpha);
+    void set_tolerance(T tolerance);//許容誤差
     T update(T target, T current);
     void reset();
 
@@ -30,6 +31,7 @@ private:
     T _prev_prev_error;
     T _low_pass_deriv;
     T _prev_output;
+    T _tolerance;
 };
 
 #endif
